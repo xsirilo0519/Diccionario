77e60f8e115dccc8b8d127eb9f5985aed3a441fa
sebas <sebaslpez0519@gmail.com>
2021-11-11 11:51:03 -0500

    Initial commit

---
+++ src/main/java/Texto.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Texto {
+
+    public Texto(){
+
+        leer("src\\main\\resources\\hola.txt");
+    }
+
+    public void leer(String nombreArchivo){
+
+        try {
+
+            FileReader r = new FileReader(nombreArchivo);
+            BufferedReader buffer = new BufferedReader(r);
+
+            String temp="";
+
+            while (temp!=null){
+                temp= buffer.readLine();
+                if (temp==null)
+                    break;
+                System.out.println(temp);
+
+
+            }
+
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+
+}
